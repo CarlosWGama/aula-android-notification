69ada315362e3fa043599bd73adebafb5538a5e7
Carlos W. Gama <carloswgama@gmail.com>
2017-10-02 15:19:33 -0300

    Projeto iniciado

---
+++ app/src/main/java/br/com/carloswgama/notification/MainActivity.java
@@ -0,0 +1,20 @@
+package br.com.carloswgama.notification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btChamaAlerta(View v) {
+
+        Toast.makeText(this, "Clicou", Toast.LENGTH_SHORT).show();
+    }
+}
